77af5cd7d966746128ec87602136b4ea31edb3dc
maratanka <anna.markowicz5@gmail.com>
2017-11-11 10:26:39 +0000

    Register/Login

--- app.js
+++ app.js
@@ -150,6 +150,20 @@ app.post("/register", function(req, res) {
     });
 });
 
+// Show login form
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "login"
+    }), function(req, res) {
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("YelpCamp Server Has Started"); 
 });
